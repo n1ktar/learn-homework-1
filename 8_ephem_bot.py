07af5ac2b22f283cadbb96943edb145c70dc88f5
n1ktar <niktarius29@gmail.com>
2022-03-02 00:41:13 +0300

    do ephem bot

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -12,8 +12,9 @@
   бота отвечать, в каком созвездии сегодня находится планета.
 
 """
+import ephem
 import logging
-
+from datetime import datetime
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
@@ -29,6 +30,10 @@ PROXY = {
     }
 }
 
+current_datetime = datetime.now()
+
+planet_dict = {'Mars': ephem.Mars(current_datetime), 'Venus': ephem.Venus(current_datetime), 'Saturn': ephem.Saturn(current_datetime), 'Jupiter': ephem.Jupiter(current_datetime),
+               'Neptune': ephem.Neptune(current_datetime), 'Uranus': ephem.Uranus(current_datetime), 'Mercury': ephem.Mercury(current_datetime), 'Moon': ephem.Moon(current_datetime), 'Pluto': ephem.Pluto(current_datetime)}
 
 def greet_user(update, context):
     text = 'Вызван /start'
@@ -36,17 +41,29 @@ def greet_user(update, context):
     update.message.reply_text(text)
 
 
+def constellations(update, context):
+    user_planet = update.message.text.split()[1]
+    print(user_planet)
+    ephem_planet = planet_dict.get(user_planet, None)
+    if ephem_planet != None:   
+        ephem_coordinates = ephem_planet
+        const = ephem.constellation(ephem_coordinates)
+        print(const)
+        update.message.reply_text(const)
+    else:
+        update.message.reply_text("I dont know this planet")
+
 def talk_to_me(update, context):
     user_text = update.message.text
     print(user_text)
-    update.message.reply_text(text)
-
+    update.message.reply_text(user_text)
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY, use_context=True)
+    mybot = Updater("5235982340:AAHh0sahvpHDbgoGCaUWFQuVB1a-3EzWjKY", request_kwargs=PROXY, use_context=True)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", constellations))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     mybot.start_polling()
